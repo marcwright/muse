fd47b2b14f8273a6b48c3652a516b66d0784051e
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2016-07-09 12:02:02 -0400

    created artist model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Artist.delete_all
+
+Artist.create(name: "Rihanna", albums: 6, hometown: "Barbados")
+Artist.create(name: "Taylor Swift", albums: 8, hometown: "Nashville")
+Artist.create(name: "Billy Joel", albums: 14, hometown: "Long Island")
+Artist.create(name: "Drake", albums: 4, hometown: "Toronto")
+Artist.create(name: "Beyonce", albums: 6, hometown: "Houston")
