24b37cafeb0ad09b45779e9cb9020862d1758aa3
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2016-07-09 12:57:44 -0400

    added artists show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "application#about"
+  root to: "artists#index"
+
+  get "about", to: "application#about"
 
   get "artists", to: "artists#index"
   get "artists/:id", to: "artists#show", as: :artist
