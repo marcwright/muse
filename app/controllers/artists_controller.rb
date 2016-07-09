51d9d1313cc9aa1c18d3b728430c2fda204ceab6
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2016-07-09 12:44:02 -0400

    artists index is working

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,13 @@
+# create a new file in app/controllers/artists_controller.rb
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+  	@artist = Artist.find(params[:id])
+  	@songs = @artist.songs
+	end
+
+end
