d00d410a9d83f04b1a9effb132b501c92757904f
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2016-07-09 12:15:37 -0400

    added has_many songs to artists and seeded the songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Artist.delete_all
-
 Artist.create(name: "Rihanna", albums: 6, hometown: "Barbados")
 Artist.create(name: "Taylor Swift", albums: 8, hometown: "Nashville")
 Artist.create(name: "Billy Joel", albums: 14, hometown: "Long Island")
 Artist.create(name: "Drake", albums: 4, hometown: "Toronto")
 Artist.create(name: "Beyonce", albums: 6, hometown: "Houston")
+
+Song.create(artist_id: 1, title: "Umbrella", genre: "Pop")
+Song.create(artist_id: 2, title: "Shake It Off", genre: "Pop")
+Song.create(artist_id: 3, title: "Pressure", genre: "Rock")
+Song.create(artist_id: 4, title: "Hotline Bling", genre: "Rap")
+Song.create(artist_id: 5, title: "Lemonade", genre: "R&B")
